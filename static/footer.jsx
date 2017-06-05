b2500bbdc6335f828e47133929b20be5734e4f8a
Justin Parker <justin@jpark.tech>
2017-06-05 15:17:26 -0400

    Move functions to footer
    No need to have it in the base component if the functionality only exists in the child component.

--- static/footer.jsx
+++ static/footer.jsx
@@ -1,14 +1,30 @@
 define(["react"], function(React) {
   return React.createClass({
+    removeComplete: function() {
+      this.props.tasks.where({done: true}).forEach(function(t) {
+        t.destroy();
+      })
+    },
+
+    markAll: function() {
+      this.props.tasks.forEach(function(t) {
+        t.set({done: true});
+      })
+    },
+
+    getCount: function() {
+      return this.props.tasks.where({done: false}).length;
+    },
+
     render: function () {
       return (
         <div className="footer">
-          <span className="count">{this.props.count} items left</span>
-          <span className="click" onClick={this.props.removeComplete}>
+          <span className="count">{this.getCount()} items left</span>
+          <span className="click" onClick={this.removeComplete()}>
             Remove completed
           </span>
           <span> | </span>
-          <span className="click" onClick={this.props.markAll}>
+          <span className="click" onClick={this.markAll()}>
             Mark all as complete
           </span>
         </div>
